05709d7e6578a2f720fa7f35702e51c935fab92a
David Wilson <davidray.real@gmail.com>
2022-09-05 18:00:58 -0500

    add random options for a gift on button click

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,15 +18,25 @@ class MainActivity : AppCompatActivity() {
             val giftText: TextView = findViewById(R.id.giftText)
 
             if (giftText.text.isNotBlank()) {
-
                 giftText.text = ""
                 giftBtn.text = "Open Gift"
             } else {
-                giftText.text = "10 whoopings for 10 years!"
+                val gift = Gift()
+                val openedGift = gift.openGift()
+                giftText.text = "You opened a $openedGift!"
                 giftBtn.text = "Close Gift"
             }
 
 //            Toast.makeText(this, "Gift Button Clicked!", Toast.LENGTH_SHORT).show()
         }
+    }
+}
+
+class Gift() {
+    private val choices: Array<String> = arrayOf("Kinder Egg", "LOL Doll", "Barbie Doll")
+
+    fun openGift(): String {
+        val randomIndex = Random.nextInt(choices.size);
+        return choices[randomIndex]
     }
 }
